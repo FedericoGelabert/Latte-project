22ffae1efe5ac042ce5d4b2c90e5d02bcfd369f5
Federico Gelabert <gelabert.fede@gmail.com>
2024-02-05 20:05:48 -0300

    Fix: Bakery image.

--- src/components/sections/Bakery.jsx
+++ src/components/sections/Bakery.jsx
@@ -2,7 +2,7 @@ const Bakery = () => {
   return (
     <div className="h-screen w-full relative" id="bakery">
       <div className="flex items-center justify-center w-full h-full">
-          <img src="" alt="Bakery Menu" className="w-2/3" />
+          <img src="" alt="Bakery Item" className="w-2/3" />
       </div>
     </div>
   )
